81178da8c5967513dfd63851ae7ba714697bacd1
Tejas Vedantham <tejas6080@gmail.com>
2019-10-03 11:46:30 -0400

    Initial commit for App

---
+++ app/src/main/java/com/example/tejasvedantham/dronedeliveryproject_clientside/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tejasvedantham.dronedeliveryproject_clientside;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
